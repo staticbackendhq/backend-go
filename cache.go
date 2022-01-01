7ebd14f97bbc5db4e162c2d806e7e7774465f626
Dominic St-Pierre <dominicstpierre@gmail.com>
2022-01-01 10:49:04 -0500

    added worker queue

--- cache.go
+++ cache.go
@@ -1,6 +1,10 @@
 package backend
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"log"
+	"time"
+)
 
 func CacheGet(token, key string, v interface{}) error {
 	var s string
@@ -21,6 +25,45 @@ func CacheSet(token, key string, v interface{}) error {
 	})
 	data.Key = key
 	data.Value = string(b)
+	var ok bool
+	return Post(token, "/sudo/cache", data, &ok)
+}
+
+type WorkerTask func(val string)
+
+// WorkerQueue monitors a work queue each 5 seconds.
+// If there's new work available it will call the WorkerTask function back.
+// This function should be ran concurrently i.e. go backend.WorkerQueue()
+func WorkerQueue(token, key string, worker WorkerTask) {
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			go checkQueue(token, key, worker)
+		}
+	}
+}
+
+func checkQueue(token, key string, worker WorkerTask) {
+	var s string
+	if err := Get(token, "/sudo/cache?type=queue&key="+key, &s); err != nil {
+		log.Println("error while checking for worker queue: ", err)
+	}
+
+	if len(s) > 0 {
+		worker(s)
+	}
+}
+
+func QueueWork(token, key, value string) error {
+	data := new(struct {
+		Key   string `json:"key"`
+		Value string `json:"value"`
+		Type  string `json:"type"`
+	})
+	data.Key = key
+	data.Value = value
+	data.Type = "queue"
 	var ok bool
 	return Post(token, "/sudo/cache", data, &ok)
 }
