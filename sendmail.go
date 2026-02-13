438cb1688faa6de2edbd34923dcb2eb72ccd31b3
Dominic St-Pierre <dominicstpierre@gmail.com>
2026-02-13 09:32:32 -0500

    added email attachments to via SendMailWithAttachements

--- sendmail.go
+++ sendmail.go
@@ -1,5 +1,13 @@
 package backend
 
+// Attachment represents an email attachment
+type Attachment struct {
+	URL         string `json:"url"`         // URL where the attachment was fetched from
+	Body        []byte `json:"body"`        // Raw bytes of the attachment
+	ContentType string `json:"contentType"` // MIME type of the attachment
+	Filename    string `json:"filename"`    // Name of the file
+}
+
 // EmailData used to request the send email process
 type EmailData struct {
 	FromName string `json:"fromName"`
@@ -8,6 +16,8 @@ type EmailData struct {
 	Subject  string `json:"subject"`
 	Body     string `json:"body"`
 	ReplyTo  string `json:"replyTo"`
+
+	Attachments []Attachment `json:"attachments"`
 }
 
 // SendMail sends an email
@@ -21,5 +31,14 @@ func SendMail(token, from, fromName, to, subject, body, replyTo string) (ok bool
 		ReplyTo:  replyTo,
 	}
 	err = Post(token, "/sudo/sendmail", data, &ok)
+	return
+}
+
+// SendMailWithAttachments sends an email with attachments. The attachments
+// either are downloaded via passing the URL or directly filling the Body,
+// ContentType, and Filename. If using only URL, ensure the URL are publicly
+// available.
+func SendMailWithAttachments(token string, email EmailData) (ok bool, err error) {
+	err = Post(token, "/sudo/sendmail", email, &ok)
 	return
 }
